869edb255a71445f5c3aea8c3a1757594732be4a
walterwan90 <walterwan1990@gmail.com>
2016-02-03 00:42:25 +0800

    Basic
    5.1

---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="col-sm-offset-2 col-sm-8">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    New Task
+                </div>
+                <div class="panel-body">
+                <!-- To Do $errors -->
+
+                    <form action="/task" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <label for="task" class="col-sm-3 control-label">Task</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="name" id="task-name" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fa fa-plus"></i> Add Task
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                @if (count($tasks) > 0)
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Current Tasks
+                        </div>
+
+                        <div class="panel-body">
+                            <table class="table table-striped task-table">
+                                <thead>
+                                    <th>Task</th>
+                                    <th>&nbsp;</th>
+                                </thead>
+                                <tbody>
+                                @foreach ($tasks as $task)
+                                    <tr>
+                                        <td class="table-text">
+                                            {{ $task->name }}
+                                        </td>
+                                        <td>
+                                            <form action="/task/{{ $task->id }}" method="POST">
+                                                {{ csrf_field() }}
+                                                {{ method_field('DELETE') }}
+
+                                                <button type="submit" class="btn btn-danger">
+                                                    <i class="fa fa-trash"></i>Delete Task
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
